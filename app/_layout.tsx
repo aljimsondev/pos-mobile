96e5dc64e95c66a11f6ed19f9c1ab23b288970c3
aljimsondev <aljimson.megrino@gmail.com>
2025-08-29 14:34:20 +0800

    Chore | added nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,44 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+// import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+// import { useFonts } from 'expo-font';
+// import { Stack } from 'expo-router';
+// import { StatusBar } from 'expo-status-bar';
+// import 'react-native-reanimated';
+
+// import { useColorScheme } from '@/hooks/useColorScheme';
+
+// export default function RootLayout() {
+//   const colorScheme = useColorScheme();
+//   const [loaded] = useFonts({
+//     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+//   });
+
+//   if (!loaded) {
+//     // Async font loading only occurs in development.
+//     return null;
+//   }
+
+//   return (
+//     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+//       <Stack>
+//         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+//         <Stack.Screen name="+not-found" />
+//       </Stack>
+//       <StatusBar style="auto" />
+//     </ThemeProvider>
+//   );
+// }
+
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Text, View } from 'react-native';
+import './global.css';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
+export default function App() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
       <StatusBar style="auto" />
-    </ThemeProvider>
+    </View>
   );
 }
