adcadabe75b547ae0b4804da6c63bc1b5369bf8a
aljimsondev <aljimson.megrino@gmail.com>
2025-08-30 11:38:59 +0800

    Chore | added better-auth client and added metro config for better-auth

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,9 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+// To resolve Better Auth exports you'll need to enable unstable_enablePackageExports in your metro config.
+config.resolver.unstable_enablePackageExports = true;
+
 module.exports = withNativeWind(config, {
   input: './app/global.css',
   inlineRem: 16,
