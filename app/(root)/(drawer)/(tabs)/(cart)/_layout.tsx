6dbc5974765332f2870be353c2ac618a40705460
aljimsondev <aljimson.megrino@gmail.com>
2025-09-04 22:15:27 +0800

    Chore | removed header

--- app/(root)/(drawer)/(tabs)/(cart)/_layout.tsx
+++ app/(root)/(drawer)/(tabs)/(cart)/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from 'expo-router';
 import React from 'react';
 
 function CartLayout() {
-  return <Stack />;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
 
 export default CartLayout;
--- components/ui/header/cart-header.tsx
+++ components/ui/header/cart-header.tsx
@@ -1,22 +1,23 @@
 import { Button } from '@/components/reusable/button';
 import { Text } from '@/components/reusable/text';
 import IconButton from '@/components/ui/IconButton';
-import { useCartSelectors } from '@/lib/store/cart-store';
+import { useCartSelectors, useCartStore } from '@/lib/store/cart-store';
 import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
-import { useColorScheme, View } from 'react-native';
+import { View } from 'react-native';
 
 export function CartHeaderRight() {
   const { isEmpty } = useCartSelectors();
-  const scheme = useColorScheme() as 'light' | 'dark';
+  const { clear, removeSelectedItems } = useCartStore();
 
   return (
     <View className="flex-1 flex-row items-center gap-2 justify-end">
       <IconButton
         icon={(color) => <Ionicons name="trash" size={18} color={color} />}
         disabled={isEmpty}
+        onPress={removeSelectedItems}
       />
-      <Button variant="outline" disabled={isEmpty}>
+      <Button variant="outline" disabled={isEmpty} onPress={clear}>
         <Text>Clear</Text>
       </Button>
     </View>
