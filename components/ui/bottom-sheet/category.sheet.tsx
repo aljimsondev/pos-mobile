27e55967369fab2ac55a972b54b566b3fe0a4a55
aljimsondev <aljimson.megrino@gmail.com>
2025-09-07 18:53:30 +0800

    Chore | created category item component

--- components/ui/bottom-sheet/category.sheet.tsx
+++ components/ui/bottom-sheet/category.sheet.tsx
@@ -2,10 +2,14 @@ import { Separator } from '@/components/reusable/separator';
 import { Text } from '@/components/reusable/text';
 import { useBottomSheetStore } from '@/lib/store/bottom-sheet.store';
 import { useCategoryStore } from '@/lib/store/category-store';
+import { THEME } from '@/lib/theme';
+import { Category } from '@aljimsondev/database-schema';
+import { Feather } from '@expo/vector-icons';
 import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
 import { BottomSheetMethods } from '@gorhom/bottom-sheet/lib/typescript/types';
+import Checkbox from 'expo-checkbox';
 import React, { useEffect, useRef } from 'react';
-import { FlatList, TouchableOpacity, View } from 'react-native';
+import { FlatList, Pressable, useColorScheme, View } from 'react-native';
 import { renderBackdrop } from './renderBackdrop';
 
 function CategorySheet() {
@@ -52,15 +56,10 @@ function CategorySheet() {
         <Separator />
         <View className="flex-1">
           <FlatList
-            data={new Array(10)}
-            renderItem={(item) => {
-              return (
-                <TouchableOpacity className="p-2 my-[2px] rounded-md">
-                  <View className="flex-1 ">
-                    <Text>Category</Text>
-                  </View>
-                </TouchableOpacity>
-              );
+            data={categories}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item, index }) => {
+              return <CategoryItem {...item} haveDescendants={index === 1} />;
             }}
           />
         </View>
@@ -70,3 +69,38 @@ function CategorySheet() {
 }
 
 export default CategorySheet;
+
+interface CategoryNodeProps {
+  isChild?: boolean;
+  isRoot?: boolean;
+  label?: string;
+}
+
+function CategoryNode({ label }: CategoryNodeProps) {
+  return (
+    <Pressable className="p-2 items-center active:bg-muted rounded-md flex-1 justify-start flex-row gap-2">
+      <Checkbox id="category_checkbox" />
+      <Text>{label}</Text>
+    </Pressable>
+  );
+}
+
+interface CategoryItemProps extends Category {
+  haveDescendants?: boolean;
+}
+function CategoryItem({ haveDescendants, name, ...rest }: CategoryItemProps) {
+  const theme = useColorScheme() as 'dark' | 'light';
+
+  const iconColor = THEME[theme].primary;
+
+  return (
+    <View className="rounded-md flex items-center flex-row gap-2">
+      <CategoryNode label={name} />
+      {haveDescendants && (
+        <Pressable className="active:bg-muted rounded-full p-2">
+          <Feather name="chevron-down" size={24} color={iconColor} />
+        </Pressable>
+      )}
+    </View>
+  );
+}
