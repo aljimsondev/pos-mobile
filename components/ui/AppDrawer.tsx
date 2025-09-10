a6bcb829443538e5c556b50d75f7fe6f897bea82
aljimsondev <aljimson.megrino@gmail.com>
2025-09-10 19:54:21 +0800

    Chore | added drawer block component

--- components/ui/AppDrawer.tsx
+++ components/ui/AppDrawer.tsx
@@ -1,7 +1,9 @@
 import { Button } from '@/components/reusable/button';
 import { Text } from '@/components/reusable/text';
 import Container from '@/components/ui/Container';
+import { DrawerBlock, DrawerBlockAction } from '@/components/ui/drawer';
 import { authClient } from '@/lib/auth/client';
+import { FontAwesome5, MaterialIcons } from '@expo/vector-icons';
 import { DrawerContentComponentProps } from '@react-navigation/drawer';
 import React from 'react';
 import { ScrollView, View } from 'react-native';
@@ -16,6 +18,32 @@ function AppDrawer({
       <ScrollView className="flex-1">
         <View className="py-3 border-b-border border-b-[1px]">
           <Text style={{ fontFamily: 'NicoMoji', fontSize: 24 }}>MENU</Text>
+        </View>
+        <View className="flex-1 mt-4">
+          <DrawerBlock blockTitle="Product">
+            <DrawerBlockAction
+              label="Create new product"
+              icon={(color, size) => (
+                <FontAwesome5 size={size} name="cart-plus" color={color} />
+              )}
+            />
+            <DrawerBlockAction
+              label="Create new brand"
+              icon={(color, size) => (
+                <MaterialIcons
+                  size={size}
+                  color={color}
+                  name="branding-watermark"
+                />
+              )}
+            />
+            <DrawerBlockAction
+              label="Add category"
+              icon={(color, size) => (
+                <MaterialIcons size={size} color={color} name="category" />
+              )}
+            />
+          </DrawerBlock>
         </View>
       </ScrollView>
       <Button onPress={() => authClient.signOut()}>
---
+++ components/ui/drawer/index.tsx
@@ -0,0 +1,42 @@
+import { Card, CardContent } from '@/components/reusable/card';
+import { Text } from '@/components/reusable/text';
+import { THEME } from '@/lib/theme';
+import { Ionicons } from '@expo/vector-icons';
+import React, { ReactNode } from 'react';
+import { Pressable, useColorScheme, View } from 'react-native';
+
+interface DrawerBlockProps {
+  children: ReactNode;
+  blockTitle: string;
+}
+
+export function DrawerBlock({ children, blockTitle }: DrawerBlockProps) {
+  return (
+    <View className="gap-2">
+      <Text className="text-muted-foreground">{blockTitle}</Text>
+      <Card className="p-2">
+        <CardContent className="gap-1 p-0">{children}</CardContent>
+      </Card>
+    </View>
+  );
+}
+
+interface DrawerActionProps {
+  label: string;
+  icon?: (color: string, size: number) => ReactNode | null;
+}
+
+export function DrawerBlockAction({ label, icon }: DrawerActionProps) {
+  const theme = useColorScheme() as 'dark' | 'light';
+  const iconColor = THEME[theme].mutedForeground;
+  const iconSize = 18;
+  return (
+    <Pressable className="flex-row p-2 active:bg-muted rounded-md items-center">
+      <View className="flex-row gap-2 items-center flex-1">
+        {icon && icon(iconColor, iconSize)}
+        <Text>{label}</Text>
+      </View>
+      <Ionicons name="chevron-forward" size={iconSize} color={iconColor} />
+    </Pressable>
+  );
+}
