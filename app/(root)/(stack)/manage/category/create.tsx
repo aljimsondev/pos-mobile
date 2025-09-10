43df0a5004a4bf0e9dfbe7cc81af2ce1a108042a
aljimsondev <aljimson.megrino@gmail.com>
2025-09-10 19:16:12 +0800

    Chore | added management pages

---
+++ app/(root)/(stack)/manage/category/create.tsx
@@ -0,0 +1,11 @@
+import { Text } from '@/components/reusable/text';
+import React from 'react';
+import { View } from 'react-native';
+
+export default function CreateCategory() {
+  return (
+    <View>
+      <Text>CreateCategory</Text>
+    </View>
+  );
+}
---
+++ app/(root)/(stack)/manage/product/create.tsx
@@ -0,0 +1,11 @@
+import { Text } from '@/components/reusable/text';
+import React from 'react';
+import { View } from 'react-native';
+
+export default function CreateProduct() {
+  return (
+    <View>
+      <Text>CreateProduct</Text>
+    </View>
+  );
+}
