dd02da44732d57d804410ff6bb174f9d8a068063
aljimsondev <aljimson.megrino@gmail.com>
2025-09-13 21:18:27 +0800

    Chore | added selection rendering

--- components/ui/bottom-sheet/measurement.sheet.tsx
+++ components/ui/bottom-sheet/measurement.sheet.tsx
@@ -1,15 +1,25 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/reusable/accordion';
+import { Label } from '@/components/reusable/label';
 import { Separator } from '@/components/reusable/separator';
 import { Text } from '@/components/reusable/text';
 import { renderBackdrop } from '@/components/ui/bottom-sheet/renderBackdrop';
+import { unit_of_measurement } from '@/constants/unit-of-measurement';
 import useSheetBackHandler from '@/hooks/useSheetBackHandler';
 import { useBottomSheetStore } from '@/lib/store/bottom-sheet.store';
 import {
   BottomSheetModal,
   BottomSheetModalProvider,
+  BottomSheetScrollView,
   BottomSheetView,
 } from '@gorhom/bottom-sheet';
+import Checkbox from 'expo-checkbox';
 import React, { useCallback, useEffect, useRef } from 'react';
-import { ScrollView } from 'react-native';
+import { View } from 'react-native';
 
 function MeasurementSheet() {
   const { measurement, close } = useBottomSheetStore();
@@ -48,24 +58,38 @@ function MeasurementSheet() {
         <BottomSheetView className="flex-1 px-4">
           <Text className="font-bold text-lg">Unit of measurement</Text>
           <Separator />
-          <ScrollView className="flex-1"></ScrollView>
+          <BottomSheetScrollView className="flex-1">
+            <Accordion type="single" collapsible>
+              {Object.entries(unit_of_measurement).map(([key, val]) => {
+                return (
+                  <AccordionItem value={key} key={key}>
+                    <AccordionTrigger>
+                      <Text>{key}</Text>
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      <View className="gap-2">
+                        {val.map((unit) => {
+                          return (
+                            <View
+                              className="flex flex-row items-center gap-3"
+                              key={unit.code}
+                            >
+                              <Checkbox id={unit.code} />
+                              <Label onPress={() => {}} htmlFor={unit.code}>
+                                {unit.name}
+                              </Label>
+                            </View>
+                          );
+                        })}
+                      </View>
+                    </AccordionContent>
+                  </AccordionItem>
+                );
+              })}
+            </Accordion>
+          </BottomSheetScrollView>
         </BottomSheetView>
       </BottomSheetModal>
-      {/* <BottomSheet
-        index={-1}
-        ref={ref}
-        snapPoints={['75%']}
-        backdropComponent={renderBackdrop}
-        onChange={onChange}
-        enablePanDownToClose
-        enableDynamicSizing={false}
-      >
-        <BottomSheetView className="flex-1">
-          <Text className="font-bold text-lg">Unit of measurement</Text>
-          <Separator />
-          <ScrollView className="flex-1"></ScrollView>
-        </BottomSheetView>
-      </BottomSheet> */}
     </BottomSheetModalProvider>
   );
 }
