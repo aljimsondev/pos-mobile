2182a9d084a56b02379cc13cfa456d7ae7c09b23
aljimsondev <aljimson.megrino@gmail.com>
2025-09-19 19:45:30 +0800

    Chore | added brand deletion handler

--- components/ui/card/BrandItem.tsx
+++ components/ui/card/BrandItem.tsx
@@ -1,10 +1,13 @@
 import { Text } from '@/components/reusable/text';
 import IconButton from '@/components/ui/IconButton';
 import useThemeVariables from '@/hooks/useThemeVariables';
+import { useDialogStore } from '@/lib/store/dialog-store';
+import { fetcher } from '@/lib/utils';
 import { Brand } from '@aljimsondev/database-schema';
 import { FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons';
 import React from 'react';
 import { View } from 'react-native';
+import { Toast } from 'toastify-react-native';
 
 interface BrandItemProps {
   brand: Brand;
@@ -12,6 +15,37 @@ interface BrandItemProps {
 
 export default function BrandItem({ brand }: BrandItemProps) {
   const colors = useThemeVariables();
+  const { showDialog } = useDialogStore();
+
+  const onDeleteBrand = () => {
+    try {
+      if (!brand.id) throw new Error('Brand id is required!');
+
+      showDialog({
+        title: 'Confirmation!',
+        description: 'Are you sure you want to remove this brand!',
+        showContinue: true,
+        showCancel: true,
+        continueText: 'Yes, delete it!',
+        variant: 'destructive',
+        onContinue: async () => {
+          const response = await fetcher(`brand/dummy`, {
+            method: 'DELETE',
+          });
+
+          const body = await response.json();
+
+          if (!body.success) throw body?.error?.message;
+
+          Toast.success('Brand deleted successfully!', 'bottom');
+        },
+      });
+    } catch (e: any) {
+      console.warn(e); // add logs for debugging purposes
+      Toast.error(e?.message, 'bottom');
+    }
+  };
+
   return (
     <View className="p-2 flex items-center flex-row gap-2">
       <View className="flex-1">
@@ -28,6 +62,7 @@ export default function BrandItem({ brand }: BrandItemProps) {
         icon={(color) => (
           <FontAwesome name="trash-o" color={colors.destructive} size={18} />
         )}
+        onPress={onDeleteBrand}
       />
     </View>
   );
