dac36bc10a4c1c229ad6c7c5e098093e363d4493
aljimsondev <aljimson.megrino@gmail.com>
2025-09-22 19:43:48 +0800

    Chore | added categories fetching

--- app/(root)/(stack)/manage/category/index.tsx
+++ app/(root)/(stack)/manage/category/index.tsx
@@ -1,8 +1,8 @@
 import NoContent from '@/components/ui/card/no-content';
-import BrandList from '@/components/ui/list/BrandList';
+import CategoriesList from '@/components/ui/list/CategoriesList';
 import SearchBar from '@/components/ui/SearchBar';
-import { fetchBrands } from '@/core/requests/fetch-brands';
-import { useInfiniteQuery } from '@tanstack/react-query';
+import { fetchCategories } from '@/core/requests/fetch-categories';
+import { useQuery } from '@tanstack/react-query';
 import debounce from 'debounce';
 import React, { useCallback, useState } from 'react';
 import { ActivityIndicator, View } from 'react-native';
@@ -14,26 +14,10 @@ export default function ManageCategory() {
   const [search, setSearch] = useState('');
   const [debouncedSearch, setDebouncedSearch] = useState('');
 
-  const { data, isFetching, fetchNextPage, refetch, isFetchingNextPage } =
-    useInfiniteQuery({
-      queryFn: ({ pageParam: nextPage }) =>
-        fetchBrands({
-          page: nextPage,
-          limit: limit,
-          search: debouncedSearch,
-          sort_by: orderBy,
-          sort_order: sortOrder,
-        }),
-      initialPageParam: 1,
-      getNextPageParam: (lastPage) => {
-        if (!lastPage.pagination.has_next_page) return undefined;
-
-        return lastPage.pagination.page + 1;
-      },
-      queryKey: [limit, debouncedSearch, sortOrder, orderBy],
-      refetchOnWindowFocus: true,
-    });
-
+  const { data, isFetching, refetch } = useQuery({
+    queryFn: () => fetchCategories(),
+    queryKey: [],
+  });
   // Debounced function to update the delayed state
   const updateDebouncedSearch = useCallback(
     debounce((searchTerm) => {
@@ -52,8 +36,8 @@ export default function ManageCategory() {
     updateDebouncedSearch(''); // Delayed update for API trigger
   };
 
-  const brands = data?.pages.map((page) => page.results).flat() || [];
-  const emptyBrands = brands.length <= 0;
+  console.log(data);
+  const noCategory = (data || []).length <= 0;
 
   return (
     <View className="p-2 flex-1">
@@ -66,7 +50,7 @@ export default function ManageCategory() {
       />
       {isFetching ? (
         <ActivityIndicator />
-      ) : emptyBrands ? (
+      ) : noCategory ? (
         <NoContent
           title="Empy brand!"
           description={
@@ -76,11 +60,10 @@ export default function ManageCategory() {
           }
         />
       ) : (
-        <BrandList
-          data={brands}
+        <CategoriesList
+          data={data || []}
           keyExtractor={(item) => item.id}
-          loadMore={fetchNextPage}
-          isFetchingData={isFetchingNextPage}
+          isFetchingData={isFetching}
           isLoading={isFetching}
           onRefresh={refetch}
           // Performance optimizations
---
+++ components/ui/list/CategoriesList.tsx
@@ -0,0 +1,65 @@
+import { Separator } from '@/components/reusable/separator';
+import { Text } from '@/components/reusable/text';
+import { Category } from '@aljimsondev/database-schema';
+import React, { Fragment } from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  FlatListProps,
+  RefreshControl,
+  View,
+} from 'react-native';
+
+export default function CategoriesList({
+  data = [],
+  isLoading = true,
+  onRefresh,
+  isFetchingData,
+  ...rest
+}: Partial<FlatListProps<Category>> & {
+  isFetchingData: boolean;
+  onRefresh?: () => void;
+  isLoading?: boolean;
+}) {
+  return (
+    <View className="flex-1">
+      <FlatList
+        {...rest}
+        data={data as Category[]}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item, index }) => {
+          return (
+            <Fragment>
+              <Text>{item.name}</Text>
+              {index !== data!.length - 1 && <Separator />}
+            </Fragment>
+          );
+        }}
+        // Pull-to-refresh functionality
+        refreshControl={
+          onRefresh ? (
+            <RefreshControl
+              refreshing={isLoading}
+              onRefresh={onRefresh}
+              colors={['#007AFF']} // Android
+              tintColor="#007AFF" // iOS
+            />
+          ) : undefined
+        }
+        // Loading indicator
+        ListFooterComponent={
+          isFetchingData ? (
+            <View className="p-2 bg-card rounded-full absolute border-muted border-[1px] bottom-0 shadow-md">
+              <ActivityIndicator size={34} />
+            </View>
+          ) : null
+        }
+        ListFooterComponentStyle={{
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      />
+    </View>
+  );
+}
