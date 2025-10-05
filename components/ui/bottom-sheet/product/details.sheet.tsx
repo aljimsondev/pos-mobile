a415147cf511c477016525d2c82153a387dda982
aljimsondev <aljimson.megrino@gmail.com>
2025-10-05 19:13:15 +0800

    Chore | added form input group

--- components/ui/bottom-sheet/product/details.sheet.tsx
+++ components/ui/bottom-sheet/product/details.sheet.tsx
@@ -3,15 +3,17 @@ import { Input } from '@/components/reusable/input';
 import { Label } from '@/components/reusable/label';
 import { Separator } from '@/components/reusable/separator';
 import { Text } from '@/components/reusable/text';
+import SelectionButton from '@/components/ui/button/selection.button';
 import useSheetBackHandler from '@/hooks/useSheetBackHandler';
 import { createProductFormSchema } from '@/lib/schema/product/create.product';
 import { useBottomSheetStore } from '@/lib/store/bottom-sheet.store';
-import { useUnitMeasurementStore } from '@/lib/store/measurement-store';
+import { useBrandStore } from '@/lib/store/brand-store';
+import { useCategoryStore } from '@/lib/store/category-store';
 import { useProductStore } from '@/lib/store/product-store';
 import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
 import { BottomSheetMethods } from '@gorhom/bottom-sheet/lib/typescript/types';
 import { zodResolver } from '@hookform/resolvers/zod';
-import React, { useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
@@ -22,13 +24,19 @@ interface ProductDetailSheetProps {}
 
 function ProductDetailSheet(props: ProductDetailSheetProps) {
   const ref = useRef<BottomSheetMethods | null>(null);
+  const { selectedCategory, setCallback, unsetCallback } = useCategoryStore();
+  const {
+    selectedBrand,
+    setCallback: setBrandCallback,
+    unsetCallback: unsetBrandCallback,
+  } = useBrandStore();
   const {
     close,
     open,
-    barcodeScanner,
+    category,
+    brand,
     productDetails: openSheet,
   } = useBottomSheetStore();
-  const { selectedMeasurement } = useUnitMeasurementStore();
   useSheetBackHandler('productDetails');
   const { selectedProduct, unsetProduct } = useProductStore();
 
@@ -65,7 +73,25 @@ function ProductDetailSheet(props: ProductDetailSheetProps) {
     }
   });
 
-  console.log(selectedProduct);
+  const onSelectCategory = useCallback(() => {
+    if (category) {
+      close('category');
+      unsetCallback(); // unset callback on dismiss sheet
+    } else {
+      open('category');
+      setCallback((category) => form.setValue('category_id', category.id));
+    }
+  }, [category]);
+
+  const onSelectBrand = useCallback(() => {
+    if (brand) {
+      close('brand');
+      unsetBrandCallback(); // unset callback on dismiss sheet
+    } else {
+      open('brand');
+      setBrandCallback((brand) => form.setValue('brand_id', brand.id));
+    }
+  }, [brand]);
 
   return (
     <BottomSheet
@@ -82,6 +108,24 @@ function ProductDetailSheet(props: ProductDetailSheetProps) {
         <Text className="font-bold text-lg">Product details</Text>
         <Separator />
         <ScrollView>
+          <View className="flex-row justify-between gap-2 w-full">
+            <View className="gap-1 flex-1">
+              <Label>Category</Label>
+              <SelectionButton
+                value={selectedCategory?.name}
+                label="Select category"
+                onPress={onSelectCategory}
+              />
+            </View>
+            <View className="gap-1 flex-1">
+              <Label>Brand</Label>
+              <SelectionButton
+                value={selectedBrand?.name}
+                label="Select brand"
+                onPress={onSelectBrand}
+              />
+            </View>
+          </View>
           <View className="gap-1 flex-1 mt-4">
             <Label>Name</Label>
             <Controller
